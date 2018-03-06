86a9e680bdcbf82f5cca264e88df53918083b7a6
Sivaranjith Sivarasa <sivara1210@osloskolen.no>
2018-03-06 12:41:09 +0100

    Init

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>NYC-reise</title>
+    <link rel="stylesheet" href="stilark/nyc-style.css">
+  </head>
+  <body>
+    <main>
+      <div class="banner"></div>
+      <nav>
+        <h3>Hjem</h3>
+        <h3>Overnatting</h3>
+        <h3>Reise dit</h3>
+        <h3>Attraksjoner</h3>
+        <h3>Om oss<h3>
+      </nav>
+
+      <div class="nav"></div>
+
+      <div id="slide"></div>
+
+      <footer>
+        	&trade; NYC-Reise.no
+      </footer>
+    </main>
+  </body>
+</html>
