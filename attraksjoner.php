a79d749e87bb092b647a8d20b4e942c157b76f7a
Sivaranjith Sivarasa <sivara1210@osloskolen.no>
2018-03-12 12:21:55 +0100

    ingenting egentlig

---
+++ attraksjoner.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>NYC-reise</title>
+    <link rel="stylesheet" href="stilark/nyc-style.css">
+  </head>
+  <body>
+    <main>
+
+      <footer>
+        	&trade; NYC-Reise.no
+      </footer>
+    </main>
+  </body>
+</html>
