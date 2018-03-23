529aa9f6d60d8b327ed56cc0c69c7b1878b2da74
William Resvoll Skaug <willir2810@osloskolen.no>
2018-03-23 15:10:25 +0100

    Meny

--- index.php
+++ index.php
@@ -3,21 +3,21 @@
   <head>
     <meta charset="utf-8">
     <title>NYC-reise</title>
-    <link rel="stylesheet" href="stilark/nyc-style.css">
+    <link rel="stylesheet" href="stilark/style.css">
     <link href='https://fonts.googleapis.com/css?family=Text Me One' rel='stylesheet'>
   </head>
   <body>
     <main>
-      <div class="banner">
-      </div>
+      <div class="container"> 
 
+      <div class="nav">
         <a href="index.php" class="btn">Hjem</a>
         <a href="overnatting.php" class="btn">Overnatting</a>
         <a href="reisedit.php" class="btn">Reise dit</a>
         <a href="attraksjoner.php" class="btn">Attraksjoner</a>
         <a href="about.php>" class="btn">Om oss</a>
+      </div>
 
-      <div class="container">
         <br><br>
         <center>
         <h2>Reise til New York.</h2>
@@ -27,11 +27,6 @@
         <img src="bilder/collage.jpg" width="80%">
         </center>
       </div>
-
-      <div id="slide">
-      </div>
-
-
     <div class="footer">
        <p>NYC-Reise &trade;</p>
     </div>
