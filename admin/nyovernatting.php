ff05440f97efeaacad9fcae8b7d3c83d86386d14
Sivaranjith Sivarasa <sivara1210@osloskolen.no>
2018-04-09 14:14:41 +0200

    nav fix

--- admin/nyovernatting.php
+++ admin/nyovernatting.php
@@ -102,7 +102,8 @@
         <a href="../overnatting.php" class="btn">Overnatting</a>
         <a href="../reisedit.php" class="btn">Reise dit</a>
         <a href="../attraksjoner.php" class="btn">Attraksjoner</a>
-        <a href="../about.php>" class="btn">Om oss</a>
+        <a href="../about.php" class="btn">Om oss</a>
+        <a href="../admin/adminindex.php" class="btn">Admin</a>
       </div>
 
       <form action="" method="POST" enctype="multipart/form-data">
