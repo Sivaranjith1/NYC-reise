1b6dc6210bee9b317d9bc8c81fba0f0fa6ee0051
Sivaranjith Sivarasa <sivara1210@osloskolen.no>
2018-04-10 19:15:56 +0200

    noen select paa attraksjon

--- attraksjoner.php
+++ attraksjoner.php
@@ -21,42 +21,42 @@
       <h1>Ting å gjøre i New York</h1>
 
         <?php
+          include_once "kobling.php";
+          //konf
+          $offset = 5; //er også limiten
 
-          include "kobling.php";
-          
-          $sql = "SELECT * FROM attraksjon ORDER BY navn ASC";
+          //total
+          $totalSql = 'SELECT COUNT(*) totalt FROM (SELECT count(*) tot FROM mydb.attraksjon_kat group by id) src;';
+          $totalt = $kobling->query($totalSql) -> fetch_assoc() ["totalt"];
+          echo "<div id='totalt'>{$totalt}</div>";
+          echo "<div id='offset'>{$offset}</div>";
+
+          //select ider
+          $sqlArray = '';
+          $sql = "SELECT id FROM mydb.attraksjon_kat group by id ORDER BY navn LIMIT {$offset};";
           $resultat = $kobling->query($sql);
+          while ($rad = $resultat -> fetch_assoc()) {
+            $id = $rad["id"];
+            $sqlArray = $sqlArray."'{$id}', ";
+          }
+          if ($sqlArray !== '') {
+            $nyArray = rtrim($sqlArray,", ");
+            $nyArray = "WHERE id IN (".$nyArray.")";
+          } else {
+            $nyArray = 'WHERE id = 0';
+          }
 
-            while ($rad = $resultat->fetch_assoc()) {
-              $attraksjonsnummer = $rad["attraksjon_nummer"];
-              $poststed = $rad["poststed"];
-              $navn = $rad["navn"];
-              $aapningstid = $rad["aapningstid"];
-              $stengetid = $rad["stengetid"];
-              $addresse = $rad["addresse"];
-              $gatenummer = $rad["gatenr"];
-              $poststed_postnummer = $rad["poststed_postnummer"];
-              $bilde = $rad["hovedbilde"];
-              $pris = $rad["pris"];
-              $beskrivelse = $rad["beskrivelse"];
-
-            
-
-          echo "<table>";
-          echo "<tr>";
-          echo "<td><img src='bilder/$bilde' alt = 'Attraksjonsbilde'></td>";
-          echo "<td>$navn</td>";
-          echo "<td>$adresse</td>";
-          echo "<td>$poststed</td>";
-          echo "</tr>";
-          echo "</table>";
-
-            }
+          //selecter elementene
+          $sql = "SELECT id FROM mydb.attraksjon_kat {$nyArray} ORDER BY navn";
+          $resultat = $kobling->query($sql);
+          while ($rad = $resultat -> fetch_assoc()) {
+            $id = $rad["id"];
+            echo $id.'<br>';
+          }
+          
         ?>
 
       </div>
-
-
     </main>
   </body>
 </html>
