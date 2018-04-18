a2c8b94fef8186a7d9ef124444dc5dfff7ce85d0
William Resvoll Skaug <willir2810@osloskolen.no>
2018-04-18 13:50:27 +0200

    WIlliam reisedit.php

---
+++ reisedit.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>NYC-reise</title>
+    <link rel="stylesheet" href="stilark/style.css">
+    <link href='https://fonts.googleapis.com/css?family=Text Me One' rel='stylesheet'>
+  </head>
+  <body>
+
+      <div class="container">
+      <div class="nav">
+        <a href="index.php" class="btn">Hjem</a>
+        <a href="overnatting.php" class="btn">Overnatting</a>
+        <a href="reisedit.php" class="btn">Reise dit</a>
+        <a href="attraksjoner.php" class="btn">Attraksjoner</a>
+        <a href="about.php" class="btn">Om oss</a>
+        <a href="admin/adminindex.php" class="btn">Admin</a>
+      </div>
+        <div class="logo">
+          <h1><u>Reisemåter</u></h1>
+        </div>
+
+        <h2>Den eneste muligheten for å komme seg til New York fra Oslo er med fly.</h2>
+        <h3>Flytur: Den beste flyreisen fra Oslo til New York, er ved en direkte-flight. Flyturen tar omtrent 8 timer, og drar ofte på en fin tid av døgnet.</h3>
+        <h3>Pris: Hvis man bestiller flytur litt i forveien, skal man ikke se bort ifra å kunne få flyturer for mellom 2000 og 3000 kroner, men hvis man reiser når det er høysesong, blant annet i fellesferiene, er det normalt med litt dyrere priser.</h3>
+        <h4>Eksempel på en flyreise fra Oslo.</h4>
+        <img src="bilder/flyreise.png">
+      </div>
+    <div class="footer">
+       <p>NYC-Reise &trade;</p>
+    </div>
+
+    <script src="js/slideshow.js"></script>
+    <script>
+      document.getElementById("ad").controls = false;
+    </script>
+  </body>
+</html>
