b2858354ac480773dbaeb7839cd841192ff50e7b
William Resvoll Skaug <willir2810@osloskolen.no>
2018-04-20 11:23:58 +0200

    W

--- reisedit.php
+++ reisedit.php
@@ -22,8 +22,8 @@
         </div>
 
         <h2>Den eneste muligheten for å komme seg til New York fra Oslo er med fly.</h2>
-        <h3>Flytur: Den beste flyreisen fra Oslo til New York, er ved en direkte-flight. Flyturen tar omtrent 8 timer, og drar ofte på en fin tid av døgnet.</h3>
-        <h3>Pris: Hvis man bestiller flytur litt i forveien, skal man ikke se bort ifra å kunne få flyturer for mellom 2000 og 3000 kroner, men hvis man reiser når det er høysesong, blant annet i fellesferiene, er det normalt med litt dyrere priser.</h3>
+        <h3><u>Flytur</u>: Den beste flyreisen fra Oslo til New York, er ved en direkte-flight. Flyturen tar omtrent 8 timer, og drar ofte på en fin tid av døgnet.</h3>
+        <h3><u>Pris</u>: Hvis man bestiller flytur litt i forveien, skal man ikke se bort ifra å kunne få flyturer for mellom 2000 og 3000 kroner, men hvis man reiser når det er høysesong, blant annet i fellesferiene, er det normalt med litt dyrere priser.</h3>
         <h4>Eksempel på en flyreise fra Oslo.</h4>
         <img src="bilder/flyreise.png">
       </div>
