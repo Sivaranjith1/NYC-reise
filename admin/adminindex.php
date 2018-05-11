1c6a6157a7cc23f6aa3c469ef34a14a2c9f5451c
Sivaranjith Sivarasa <sivara1210@osloskolen.no>
2018-05-11 16:38:57 +0200

    admin dropdown lagt til

--- admin/adminindex.php
+++ admin/adminindex.php
@@ -16,10 +16,29 @@
       <div class="container">
       <div class="nav">
         <a href="../index.php" class="btn">Tilbake</a>
-        <a href="nyAttraksjon.php" class="btn">Legg til Attraksjon</a>
-        <a href="alleAttraksjoner.php" class="btn">Endre / slett Attraksjon</a>
+
+        <div class="btn dropdown"><span>Attraksjon</span>
+          <div class="dropdown-content">
+            <a href="nyAttraksjon.php" class="btn">Legg til Attraksjon</a>
+            <a href="alleAttraksjoner.php" class="btn">Endre / slett Attraksjon</a>
+          </div>
+        </div>
+
+        <div class="btn dropdown"><span>Overnatting</span>
+          <div class="dropdown-content">
+            <a href="nyovernatting.php" class="btn">Ny Overnatting</a>
+            <a href="alleAttraksjoner.php" class="btn">Endre / slett Overnatting</a>
+          </div>
+        </div>
+
+        <div class="btn dropdown"><span>Spisested</span>
+          <div class="dropdown-content">
+            <a href="nyovernatting.php" class="btn">Ny Spisested</a>
+            <a href="alleAttraksjoner.php" class="btn">Endre / slett Spisested</a>
+          </div>
+        </div>
+
         <a href="velgSlide.php" class="btn">bildefremvisning</a>
-        <a href="nyovernatting.php" class="btn">Legg til Overnatting</a>
         <a href="nyAdmin.php" class="btn">Ny admin</a>
       </div>
         <div class="logo">
